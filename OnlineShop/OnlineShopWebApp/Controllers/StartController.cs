9cc055bac02cacfe4f7cdea09e10770edcc03401
Pavel Dikunets <biodubstep@gmail.com>
2021-10-10 14:06:33 +0300

    lesson2_1_1 completed

---
+++ OnlineShop/OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            DateTime Time = DateTime.Now;
+            if (Time.Hour >= 0 && Time.Hour < 6)
+            {
+                return "Доброй ночи";
+            }
+            if (Time.Hour >= 6 && Time.Hour < 12)
+            {
+                return "Доброе утро";
+            }
+            if (Time.Hour >= 12 && Time.Hour < 18)
+            {
+                return "Добрый день";
+            }
+            else return "Добрый вечер";
+        }
+    }
+}
