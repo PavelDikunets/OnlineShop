0449b9e9e75f4224054ea02b6abc5201dacde201
Pavel Dikunets <biodubstep@gmail.com>
2021-10-11 19:34:13 +0300

    lesson2_1_4 completed

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (!string.IsNullOrEmpty(c))
+            {
+                switch (c)
+                {
+                    case "+":
+                        return $"{a} + {b} = {a + b}";
+                    case "-":
+                        return $"{a} - {b} = {a - b}";
+                    case "*":
+                        return $"{a} * {b} = {a * b}";
+                    case "/":
+                        if (b == 0) return $"На ноль делить нелья!";
+                        return $"{a} / {b} = {a / b}";
+                    default:
+                        return "Неверная операция!\nДоступные операции:\n+ сложение\n- вычитание\n* умножение\n/ деление";
+                }
+            }
+            else return $"{a} + {b} = {a + b}";
+        }
+    }
+}
