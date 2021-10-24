1113cbd09d40bf682374b5facfdb1909eff7a5ce
Pavel Dikunets <biodubstep@gmail.com>
2021-10-24 23:12:41 +0300

    Исправление в CartController, добавление зависимостей. CartsStorage.cs - избавление от статических методов

--- OnlineShop/OnlineShopWebApp/CartsStorage.cs
+++ OnlineShop/OnlineShopWebApp/CartsStorage.cs
@@ -7,14 +7,14 @@ namespace OnlineShopWebApp
 {
     public class CartsStorage
     {
-        private static List<Cart> carts = new List<Cart>();
+        private List<Cart> carts = new List<Cart>();
 
-        public static Cart TryGetByUserId(string userId)
+        public Cart TryGetByUserId(string userId)
         {
             return carts.FirstOrDefault(x => x.UserId == userId);
         }
 
-        public static void Add(Product product, string userId)
+        public void Add(Product product, string userId)
         {
             var currentCart = TryGetByUserId(userId);
             if (currentCart == null)
--- OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -5,21 +5,23 @@ namespace OnlineShopWebApp.Controllers
     public class CartController : Controller
     {
         private readonly ProductsStorage productsStorage;
+        private readonly CartsStorage cartsStorage;
 
-        public CartController(ProductsStorage productsStorage)
+        public CartController(ProductsStorage productsStorage, CartsStorage cartsStorage)
         {
             this.productsStorage = productsStorage;
+            this.cartsStorage = cartsStorage;
         }
         public IActionResult Index()
         {
-            var cart = CartsStorage.TryGetByUserId(Constants.UserId);
+            var cart = cartsStorage.TryGetByUserId(Constants.UserId);
             return View(cart);
         }
 
         public IActionResult Add(int productId)
         {
             var product = productsStorage.TryGetById(productId);
-            CartsStorage.Add(product, Constants.UserId);
+            cartsStorage.Add(product, Constants.UserId);
             return RedirectToAction("Index");
         }
     }
