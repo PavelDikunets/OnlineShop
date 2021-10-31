32a8f3d94a6b9e79aa9a9ddb743853965cbecebd
DeveloperCramp <biodubstep@gmail.com>
2021-10-31 18:35:04 +0300

    Добавлен ViewComponent\Cart В _Layout добавлено отображеие кол-во товаров в корзине через ViewComponent

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Cart
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartsStorage cartsStorage;
+
+        public CartViewComponent(ICartsStorage cartsStorage)
+        {
+            this.cartsStorage = cartsStorage;
+        }
+
+        public IViewComponentResult Invoke() 
+        {
+            var cart = cartsStorage.TryGetByUserId(Constants.UserId);
+            var productCounts = cart?.Amount ?? 0;
+            return View("Cart", productCounts);
+        }
+    }
+}
