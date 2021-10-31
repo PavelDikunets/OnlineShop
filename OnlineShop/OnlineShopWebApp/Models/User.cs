4514688a02fc5b924837486ea69b127ffec197fd
DeveloperCramp <biodubstep@gmail.com>
2021-10-31 15:41:46 +0300

    Добавлен класс User, содержащий свойства контактной информации пользователя. Изменено представление Order'a - Index для передачи контактной информации от пользователя.

---
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email {get;set;}
+
+    }
+}
