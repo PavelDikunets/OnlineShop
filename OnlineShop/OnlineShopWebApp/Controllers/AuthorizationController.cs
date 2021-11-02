641c1b18fa1fc8aaca3da9186c54d12b55bf13e3
DeveloperCramp <biodubstep@gmail.com>
2021-11-03 00:09:24 +0300

    Контроллер Sign_in переименован в Authorization Добавлен метож Sign_up принимающий из View Sign_up данные регистрации нового пользователя: логин, пароль, подтверждение пароля

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sign_in(string login, string password, bool remember)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Sign_up(string login, string password, string repeatPassword)
+        {
+            return View();
+        }
+    }
+}
