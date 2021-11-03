5177c687652a515c75ecd4fa6f3c95379cf5a793
DeveloperCramp <biodubstep@gmail.com>
2021-11-03 23:50:11 +0300

    Добавлен контроллер CompareController с методом Add для добавления товара по Id для сравнения

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CompareController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CompareController : Controller
+    {
+        private readonly IProductsStorage productsStorage;
+
+        public CompareController(IProductsStorage productsStorage)
+        {
+            this.productsStorage = productsStorage;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add(int productId)
+        {
+            var product = productsStorage.TryGetById(productId);
+            return RedirectToAction("Index");
+        }
+    }
+}
