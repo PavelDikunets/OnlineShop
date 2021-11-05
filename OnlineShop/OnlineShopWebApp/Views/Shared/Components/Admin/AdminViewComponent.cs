3c5bc4810ff54f2ceb81255df31f62d05f256b1d
DeveloperCramp <biodubstep@gmail.com>
2021-11-05 18:14:57 +0300

    Отдельное представление навигации в Панели администратора

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Admin/AdminViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Admin
+{
+    public class AdminViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Admin");
+        }
+    }
+}
