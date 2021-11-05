f6f5d705d6a6ac33aca5d63943d1b81775a90884
DeveloperCramp <biodubstep@gmail.com>
2021-11-05 23:11:24 +0300

    Убрал конструктор

--- OnlineShop/OnlineShopWebApp/InMemoryProductsStorage.cs
+++ OnlineShop/OnlineShopWebApp/InMemoryProductsStorage.cs
@@ -8,9 +8,26 @@ namespace OnlineShopWebApp
     {
         private List<Product> products = new List<Product>()
         {
-            new Product("Ноутбук Xiaomi Mi Notebook Air 13.3\"", 79900, "\nПроцессор и память:\nПроцессор: Intel i5-8250U\nКоличество ядер: Quad-Core\nЧастота: 3.4 ГГц\nОперативная память: 8 Гб\nВидеокарта: GeForce MX150 VRAM GDDR5 объемом 2 ГБ\n\nДисплей:\nДиагональ экрана: 13.3\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 210,9\nТолщина (мм): 14,8\nДлина (мм): 309,6\nВес (г): 1300"),
-            new Product("Ноутбук ASUS ExpertBook B5 13.3\"", 81990, "\nПроцессор и память:\nПроцессор: Intel I5-1135G7\nКоличество ядер: Quad-Core\nЧастота: 2.4 ГГц\nОперативная память: 8 Гб\nВидеокарта: Intel Iris Xe Graphics\n\nДисплей:\nДиагональ экрана: 13.3\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 210,6\nТолщина (мм): 16,9\nДлина (мм): 309,9\nВес (г): 1200"),
-            new Product("Ноутбук DELL Vostro 3500, 15.6\"", 35990, "\nПроцессор и память:\nПроцессор: Intel Core i3 1115G4\nКоличество ядер процессора: Dual-Core\nЧастота: 3.0ГГц\nОперативная память: 8 Гб\nВидеокарта: Intel UHD Graphics\n\nДисплей:\nДиагональ экрана: 15.6\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 363.96\nТолщина (мм): 19\nДлина (мм): 249\nВес (г): 1780"),
+            new Product
+            {   Id = 0,
+                Name = "Ноутбук Xiaomi Mi Notebook Air 13.3\"",
+                Cost = 79900,
+                Description = "\nПроцессор и память:\nПроцессор: Intel i5-8250U\nКоличество ядер: Quad-Core\nЧастота: 3.4 ГГц\nОперативная память: 8 Гб\nВидеокарта: GeForce MX150 VRAM GDDR5 объемом 2 ГБ\n\nДисплей:\nДиагональ экрана: 13.3\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 210,9\nТолщина (мм): 14,8\nДлина (мм): 309,6\nВес (г): 1300"
+            },
+            new Product
+            {
+                Id = 1,
+                Name = "Ноутбук ASUS ExpertBook B5 13.3\"",
+                Cost = 81990,
+                Description = "\nПроцессор и память:\nПроцессор: Intel I5-1135G7\nКоличество ядер: Quad-Core\nЧастота: 2.4 ГГц\nОперативная память: 8 Гб\nВидеокарта: Intel Iris Xe Graphics\n\nДисплей:\nДиагональ экрана: 13.3\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 210,6\nТолщина (мм): 16,9\nДлина (мм): 309,9\nВес (г): 1200"
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Ноутбук DELL Vostro 3500, 15.6\"",
+                Cost = 35990,
+                Description = "\nПроцессор и память:\nПроцессор: Intel Core i3 1115G4\nКоличество ядер процессора: Dual-Core\nЧастота: 3.0ГГц\nОперативная память: 8 Гб\nВидеокарта: Intel UHD Graphics\n\nДисплей:\nДиагональ экрана: 15.6\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 363.96\nТолщина (мм): 19\nДлина (мм): 249\nВес (г): 1780"
+            },
         };
         public List<Product> GetAll()
         {
@@ -20,26 +37,13 @@ namespace OnlineShopWebApp
         {
             return products.FirstOrDefault(product => product.Id == id);
         }
-        public void Edit(int id, string name, decimal cost, string description)
-        {
-            var product = products.FirstOrDefault(x => x.Id == id);
-            product.Name = name;
-            product.Cost = cost;
-            product.Description = description;
-        }
         public void Remove(int id)
         {
             var product = products.FirstOrDefault(x => x.Id == id);
             products.Remove(product);
         }
-        public void Add(string name, decimal cost, string description)
+        public void Add(Product product)
         {
-            var product = new Product(name, cost, description)
-            {
-                Name = name,
-                Cost = cost,
-                Description = description
-            };
             products.Add(product);
         }
     }
--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -2,18 +2,9 @@
 {
     public class Product
     {
-        private static int InstanceCounter = 0;
-        public int Id { get; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Cost { get; set; }
         public string Description { get; set; }
-        public Product(string name, decimal cost, string description)
-        {
-            Id = InstanceCounter;
-            Name = name;
-            Cost = cost;
-            Description = description;
-            InstanceCounter += 1;
-        }
     }
 }
