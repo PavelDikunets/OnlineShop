366bf7d73db6fffd25b1ffe7bf43f40b21c88c6d
DeveloperCramp <biodubstep@gmail.com>
2021-11-05 23:21:10 +0300

    Доработки

--- OnlineShop/OnlineShopWebApp/IProductsStorage.cs
+++ OnlineShop/OnlineShopWebApp/IProductsStorage.cs
@@ -7,8 +7,7 @@ namespace OnlineShopWebApp
     {
         List<Product> GetAll();
         Product TryGetById(int id);
-        void Edit(int id, string name, decimal cost, string description);
         void Remove(int id);
-        void Add(string name, decimal cost, string description);
+        void Add(Product product);
     }
 }
