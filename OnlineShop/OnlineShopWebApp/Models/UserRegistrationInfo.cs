5000afef38512956eb83f234dcfd97ee8a3cd49d
DeveloperCramp <biodubstep@gmail.com>
2021-11-06 23:18:36 +0300

    Модель с валидацией данных регистрации юзера

---
+++ OnlineShop/OnlineShopWebApp/Models/UserRegistrationInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class UserRegistrationInfo
+    {
+        [Required(ErrorMessage = "Заполните поле")]
+        [EmailAddress(ErrorMessage = "E-mail указан неверно")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
