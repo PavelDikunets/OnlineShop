6dc19d4b85d97b7255f93c1c32a134e1da000feb
DeveloperCramp <biodubstep@gmail.com>
2021-11-11 22:53:49 +0300

    Добавлен Guid Id

--- OnlineShop/OnlineShopWebApp/Models/Order.cs
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -6,6 +6,7 @@ namespace OnlineShopWebApp.Models
 {
     public class Order
     {
+        public Guid Id { get; set; }
         private static int InstanceCounter = 0;
         public int Number { get; set; }
         public UserDeliveryInfo User { get; set; }
@@ -21,6 +22,7 @@ namespace OnlineShopWebApp.Models
         }
         public Order()
         {
+            Id = Guid.NewGuid();
             Number = InstanceCounter;
             InstanceCounter += 1;
             Status = "Создан";
