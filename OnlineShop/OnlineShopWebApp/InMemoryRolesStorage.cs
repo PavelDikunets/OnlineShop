f80fbde2527ee043845ea35539d2a6893abb47eb
DeveloperCramp <biodubstep@gmail.com>
2021-11-16 13:08:47 +0300

    Добавлено хранилище InMemoryRolesStorage

---
+++ OnlineShop/OnlineShopWebApp/InMemoryRolesStorage.cs
@@ -0,0 +1,30 @@
+﻿using OnlineShopWebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShopWebApp
+{
+    public class InMemoryRolesStorage : IRolesStorage
+    {
+        private readonly List<Role> roles = new List<Role>();
+        public void Add(Role role)
+        {
+            roles.Add(role);
+        }
+
+        public List<Role> GetAll()
+        {
+            return roles;
+        }
+
+        public Role TryGetByName(string name)
+        {
+            return roles.FirstOrDefault(x => x.Name == name);
+        }
+
+        public void Remove(string roleName)
+        {
+            roles.RemoveAll(x => x.Name == roleName);
+        }
+    }
+}
