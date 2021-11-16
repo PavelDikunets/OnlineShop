a4d4bfbbe2fff44ab309c457eac642db60cbeb83
DeveloperCramp <biodubstep@gmail.com>
2021-11-16 13:06:04 +0300

    Добавлена модель Role с валидацией

---
+++ OnlineShop/OnlineShopWebApp/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Role
+    {
+        [Required(ErrorMessage = "Заполните поле")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Название роли должно быть не меньше 5 и не больше 20 символов")]
+        [RegularExpression(@"[a-zA-Z-]+$", ErrorMessage = "Название роли только на английской раскладке, без символов и цифр")]
+        public string Name { get; set; }
+    }
+}
