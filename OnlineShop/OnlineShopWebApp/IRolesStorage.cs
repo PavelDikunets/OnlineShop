9898430b044ab926de1a90e64aad7ecac9ca3356
DeveloperCramp <biodubstep@gmail.com>
2021-11-16 13:08:04 +0300

    Добавлен интерфейс IRolesStorage

---
+++ OnlineShop/OnlineShopWebApp/IRolesStorage.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OnlineShopWebApp
+{
+    public interface IRolesStorage
+    {
+        void Add(Role role);
+        List<Role> GetAll();
+        Role TryGetByName(string name);
+        void Remove(string name);
+    }
+}
