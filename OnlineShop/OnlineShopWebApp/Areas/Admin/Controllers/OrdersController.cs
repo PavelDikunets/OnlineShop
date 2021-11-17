f6f6a103652bbb7d579f62cc72ebca88e7f37d68
DeveloperCramp <biodubstep@gmail.com>
2021-11-17 13:27:21 +0300

    Отдельный контроллер Order и представления

---
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace OnlineShopWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly IOrdersStorage ordersStorage;
+
+        public OrdersController(IProductsStorage productsStorage, IOrdersStorage ordersStorage, IRolesStorage rolesStorage)
+        {
+            this.ordersStorage = ordersStorage;
+        }
+        public IActionResult Index()
+        {
+            var orders = ordersStorage.GetAll();
+            return View(orders);
+        }
+        public IActionResult OrderDetails(Guid id)
+        {
+            var order = ordersStorage.TryGetByOrderId(id);
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateStatus(Guid orderId, string status)
+        {
+            ordersStorage.UpdateStatus(orderId, status);
+            return RedirectToAction("Orders");
+        }
+    }
+}
