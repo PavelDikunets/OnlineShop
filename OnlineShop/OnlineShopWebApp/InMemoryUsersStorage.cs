68665d9eea6af0aafc7aa905ed6c3bb9546d562f
DeveloperCramp <biodubstep@gmail.com>
2021-11-17 16:41:54 +0300

    Добавление хранилища зарегистрированных пользователей

---
+++ OnlineShop/OnlineShopWebApp/InMemoryUsersStorage.cs
@@ -0,0 +1,25 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShopWebApp
+{
+    public class InMemoryUsersStorage : IUsersStorage
+    {
+        private readonly List<UserRegistrationInfo> users = new List<UserRegistrationInfo>();
+        public void Add(UserRegistrationInfo user)
+        {
+            users.Add(user);
+        }
+
+        public List<UserRegistrationInfo> GetAll()
+        {
+            return users;
+        }
+        public UserRegistrationInfo TryGetById(Guid userId)
+        {
+            return users.FirstOrDefault(x => x.Id == userId);
+        }
+    }
+}
