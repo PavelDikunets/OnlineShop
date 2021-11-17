b2fa44164ccaff7a2d9eb0b23597f9a08a134d39
DeveloperCramp <biodubstep@gmail.com>
2021-11-17 16:42:15 +0300

    Добавлен интерфейс UsersStorage

---
+++ OnlineShop/OnlineShopWebApp/IUsersStorage.cs
@@ -0,0 +1,13 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OnlineShopWebApp
+{
+    public interface IUsersStorage
+    {
+        void Add(UserRegistrationInfo user);
+        List<UserRegistrationInfo> GetAll();
+        UserRegistrationInfo TryGetById(Guid userId);
+    }
+}
