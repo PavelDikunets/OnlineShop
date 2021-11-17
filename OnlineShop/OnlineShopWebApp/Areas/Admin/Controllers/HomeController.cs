ff08310830e2f203b8858cfc8fd786db9a0c4593
DeveloperCramp <biodubstep@gmail.com>
2021-11-17 13:26:54 +0300

    Отдельный контроллер Home и представление

---
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Areas
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
