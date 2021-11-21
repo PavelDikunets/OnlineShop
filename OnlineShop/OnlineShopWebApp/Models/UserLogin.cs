a692463a3517e36f9571ba588c0709fe3c47dd7f
DeveloperCramp <biodubstep@gmail.com>
2021-11-21 13:13:36 +0300

    переименовывание модели UserLoginInfo в UserLogin переименовывание Views

---
+++ OnlineShop/OnlineShopWebApp/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Заполните поле")]
+        [EmailAddress(ErrorMessage = "E-mail указан неверно")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
+        public string Password { get; set; }
+        public bool IsRemember { get; set; }
+    }
+}
