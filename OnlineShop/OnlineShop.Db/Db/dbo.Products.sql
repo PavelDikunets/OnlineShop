b72b465c1f22a24e8fa18c3d3a4db91bef42829c
DeveloperCramp <biodubstep@gmail.com>
2021-11-28 00:22:21 +0300

    удаление ненужного)

--- OnlineShop/OnlineShop.Db/Db/dbo.Products.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Products] (
-    [Id]          UNIQUEIDENTIFIER NOT NULL,
-    [Name]        NVARCHAR (MAX)   NULL,
-    [Cost]        MONEY  NOT NULL,
-    [Description] NVARCHAR (MAX)   NULL,
-    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
