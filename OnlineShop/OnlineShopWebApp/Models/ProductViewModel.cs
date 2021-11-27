4d4e298488a960588f3f57d6049d2ef93d7c2b2b
DeveloperCramp <biodubstep@gmail.com>
2021-11-27 17:49:03 +0300

    переименовывание модели для отображения, Id теперь имеет тип Guid

--- OnlineShop/OnlineShopWebApp/Models/ProductViewModel.cs
+++ OnlineShop/OnlineShopWebApp/Models/ProductViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineShopWebApp.Models
 {
-    public class Product
+    public class ProductViewModel
     {
-        private static int InstanceCounter = 0;
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
         [Required(ErrorMessage = "Заполните поле")]
         [StringLength(50, MinimumLength = 15, ErrorMessage = "Наименование продукта не может быть меньше 15 символов")]
@@ -20,10 +21,5 @@ namespace OnlineShopWebApp.Models
         [StringLength(400, MinimumLength = 50, ErrorMessage = "Описание продукта не может быть меньше 50 символов")]
         [RegularExpression(@"[^`~!@#$%^&*_=+<>|\/?;№]+$", ErrorMessage = "Пожалуйста, для указания описания продукта не используйте следующие символы: ` ~ ! @ # $ % ^ & * _ = + | < > / ? ; № ")]
         public string Description { get; set; }
-        public Product()
-        {
-            Id = InstanceCounter;
-            InstanceCounter += 1;
-        }
     }
 }
