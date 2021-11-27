3c9c00d04752f5f07700619023def328c957e7e9
DeveloperCramp <biodubstep@gmail.com>
2021-11-27 17:58:05 +0300

    Mapping замена int на Guid

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Models;
+using System;
 
 namespace OnlineShopWebApp.Controllers
 {
@@ -10,10 +13,17 @@ namespace OnlineShopWebApp.Controllers
         {
             this.productsStorage = productsStorage;
         }
-        public IActionResult Index(int productId)
+        public IActionResult Index(Guid productId)
         {
             var product = productsStorage.TryGetById(productId);
-            return View(product);
+            var productViewModel = new ProductViewModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Cost = product.Cost,
+                Description = product.Description
+            };
+            return View(productViewModel);
         }
     }
 }
