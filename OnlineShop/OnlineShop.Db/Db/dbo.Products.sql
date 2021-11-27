af21d11835d7a039e23a447b219e749688aee589
DeveloperCramp <biodubstep@gmail.com>
2021-11-27 18:01:56 +0300

    Не знаю, куда сохранить :)

---
+++ OnlineShop/OnlineShop.Db/Db/dbo.Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [Name]        NVARCHAR (MAX)   NULL,
+    [Cost]        MONEY  NOT NULL,
+    [Description] NVARCHAR (MAX)   NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
