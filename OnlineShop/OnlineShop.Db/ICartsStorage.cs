286dcbd744943a60f20949a9ecd5667fe0392ba0
DeveloperCramp <biodubstep@gmail.com>
2021-11-28 00:05:30 +0300

    перемещение интерфейса Carts в Db

---
+++ OnlineShop/OnlineShop.Db/ICartsStorage.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db
+{
+    public interface ICartsStorage
+    {
+        void Add(Product product, string userId);
+        void DecreaseAmount(Product product, string userId);
+        void Clear(string userId);
+        Cart TryGetByUserId(string userId);
+    }
+}
