d04bbee70465708a8351511c6b071dffca088cdb
DeveloperCramp <biodubstep@gmail.com>
2021-11-27 17:51:39 +0300

    mapping products

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Models;
+using System.Collections.Generic;
 
 namespace OnlineShopWebApp.Controllers
 {
@@ -13,7 +16,19 @@ namespace OnlineShopWebApp.Controllers
         public IActionResult Index()
         {
             var products = productsStorage.GetAll();
-            return View(products);
+            var productsViewModels = new List<ProductViewModel>();
+            foreach (var product in products)
+            {
+                var productViewModel = new ProductViewModel
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Cost = product.Cost,
+                    Description = product.Description
+                };
+                productsViewModels.Add(productViewModel);
+            }
+            return View(productsViewModels);
         }
     }
 }
