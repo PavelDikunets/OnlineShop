02afdc95886f3cd262e58fc0ac0bc4dc0b947133
DeveloperCramp <biodubstep@gmail.com>
2021-11-28 20:26:28 +0300

    перенос повыше)

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -5,13 +5,13 @@ namespace OnlineShop.Db
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> Items { get; set; }
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
             Database.EnsureCreated();
         }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<CartItem> Items { get; set; }
     }
 }
