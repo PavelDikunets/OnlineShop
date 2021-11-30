a2da906e75eb875c0f68a472d7e51b01387a8156
DeveloperCramp <biodubstep@gmail.com>
2021-11-30 21:33:15 +0300

    Список продуктов

--- OnlineShop/OnlineShop.Db/Models/FavoriteProduct.cs
+++ OnlineShop/OnlineShop.Db/Models/FavoriteProduct.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OnlineShop.Db.Models
 {
-   public class FavoriteProduct
+    public class FavoriteProduct
     {
         public Guid Id { get; set; }
         public string UserId { get; set; }
-        public Product Product { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
