07820dff76f74990c63df0cf994ea2263023061e
DeveloperCramp <biodubstep@gmail.com>
2021-11-30 21:33:46 +0300

    добавление DbSet'a Избранных продуктов

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -8,6 +8,7 @@ namespace OnlineShop.Db
         public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> Items { get; set; }
+        public DbSet<FavoriteProduct> FavoriteProducts { get; set; }
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
