94715000e037af7ae14c8b73ce192122b50cf4fe
DeveloperCramp <biodubstep@gmail.com>
2021-12-02 21:14:15 +0300

    Компонент для отображения кол-ва избранных товаров в header'e рядом с кнопкой "Избранное" в навигационном меню

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/FavoriteProductsCount/FavoriteProductsCountViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Helpers;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Cart
+{
+    public class FavoriteProductsCountViewComponent : ViewComponent
+    {
+        private readonly IFavoriteStorage favoriteStorage;
+
+        public FavoriteProductsCountViewComponent(IFavoriteStorage favoriteStorage)
+        {
+            this.favoriteStorage = favoriteStorage;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var productsCount = favoriteStorage.GetAll(Constants.UserId).Count;
+            return View("FavoriteProductsCount", productsCount);
+        }
+    }
+}
