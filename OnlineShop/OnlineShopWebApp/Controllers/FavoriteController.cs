6e14a86ec8499baa295566f70ef94e12b3267fd5
DeveloperCramp <biodubstep@gmail.com>
2021-12-02 21:12:29 +0300

    добавлен контроллер для избранных товаров у конкретного пользователя с реализацией : - добавления товара в избранное - удаление конкретного товара - очистка всех продуктов в избранном

---
+++ OnlineShop/OnlineShopWebApp/Controllers/FavoriteController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Helpers;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        private readonly IFavoriteStorage favoriteProductStorage;
+        private readonly IProductsStorage productsStorage;
+
+        public FavoriteController(IFavoriteStorage favoriteProductStorage, IProductsStorage productsStorage)
+        {
+            this.favoriteProductStorage = favoriteProductStorage;
+            this.productsStorage = productsStorage;
+        }
+
+        public IActionResult Index()
+        {
+            var products = favoriteProductStorage.GetAll(Constants.UserId);
+            return View(Mapping.ToProductViewModels(products));
+        }
+
+        public IActionResult Add(Guid productId)
+        {
+            var product = productsStorage.TryGetById(productId);
+            favoriteProductStorage.Add(Constants.UserId, product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(Guid productId)
+        {
+            favoriteProductStorage.Remove(Constants.UserId, productId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Clear(string userId)
+        {
+            favoriteProductStorage.Clear(Constants.UserId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
