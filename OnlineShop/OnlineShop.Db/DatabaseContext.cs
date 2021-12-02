257b69bb04f2f95d467f3808ba1e882f9d2be7df
DeveloperCramp <biodubstep@gmail.com>
2021-12-02 23:54:04 +0300

    Подключение DbSet'a для сравнения товаров

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -9,6 +9,7 @@ namespace OnlineShop.Db
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> Items { get; set; }
         public DbSet<FavoriteProduct> FavoriteProducts { get; set; }
+        public DbSet<CompareProduct> CompareProducts { get; set; }
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
