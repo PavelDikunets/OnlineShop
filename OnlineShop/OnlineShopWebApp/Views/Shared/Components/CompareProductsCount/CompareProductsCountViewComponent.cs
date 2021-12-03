842d58bff8af1bc44a42a4479b3334dbf01230e0
DeveloperCramp <biodubstep@gmail.com>
2021-12-03 21:42:13 +0300

    Компонент для отображение кол-ва товаров в сравнении

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/CompareProductsCount/CompareProductsCountViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Helpers;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Cart
+{
+    public class CompareProductsCountViewComponent : ViewComponent
+    {
+        private readonly ICompareStorage compareStorage;
+
+        public CompareProductsCountViewComponent(ICompareStorage compareStorage)
+        {
+            this.compareStorage = compareStorage;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var productsCount = compareStorage.GetAll(Constants.UserId).Count;
+            return View("CompareProductsCount", productsCount);
+        }
+    }
+}
