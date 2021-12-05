122c6139face54bda11a9452c726aa7830e74ce9
DeveloperCramp <biodubstep@gmail.com>
2021-12-05 13:14:49 +0300

    Создан класс UserDeliveryInfo для БД

---
+++ OnlineShop/OnlineShop.Db/Models/UserDeliveryInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OnlineShop.Db.Models
+{
+    public class UserDeliveryInfo
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Comments { get; set; }
+    }
+}
