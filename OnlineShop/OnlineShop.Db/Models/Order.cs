c53e8e06ea1956fd7a1cecd6f003f82b625e9e0e
DeveloperCramp <biodubstep@gmail.com>
2021-12-05 13:14:22 +0300

    Создан клас Order для БД

---
+++ OnlineShop/OnlineShop.Db/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Db.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public UserDeliveryInfo UserDeliveryInfo { get; set; }
+        public List<CartItem> Items { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public Order()
+        {
+            CreateDateTime = DateTime.Now;
+            Status = OrderStatus.Created;
+        }
+    }
+}
