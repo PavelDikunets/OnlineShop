d0fa25ee0bc20bf8da12495c6c75bcd54ec69b0c
DeveloperCramp <biodubstep@gmail.com>
2021-12-05 13:16:08 +0300

    Интерфейс для OrdersStorage

---
+++ OnlineShop/OnlineShop.Db/IOrdersStorage.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Db.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OnlineShopWebApp
+{
+    public interface IOrdersStorage
+    {
+        void Add(Order order);
+        List<Order> GetAll();
+        Order TryGetById(Guid id);
+        void UpdateStatus(Guid orderId, OrderStatus status);
+    }
+}
