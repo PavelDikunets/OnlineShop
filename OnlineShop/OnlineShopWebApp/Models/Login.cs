1b0fac0b812946dc63e760f6a7b4265e43a2b968
DeveloperCramp <biodubstep@gmail.com>
2022-02-06 11:20:16 +0300

    Добавление класса Login

---
+++ OnlineShop/OnlineShopWebApp/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Заполните поле")]
+        [EmailAddress(ErrorMessage = "E-mail указан неверно")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
