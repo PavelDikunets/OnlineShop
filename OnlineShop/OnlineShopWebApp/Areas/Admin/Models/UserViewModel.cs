5eafe31a280f7e88c8fa62881178d102d8144c20
DeveloperCramp <biodubstep@gmail.com>
2022-02-06 11:21:43 +0300

    Модель для отображения пользователя

---
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
