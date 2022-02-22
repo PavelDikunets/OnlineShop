434a7dded0bd1ec00c4c19e8d2cded42c2ce7c29
DeveloperCramp <biodubstep@gmail.com>
2022-02-22 22:14:20 +0300

    Модель смены пароля

---
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Areas.Admin.Models
+{
+    public class ChangePasswordViewModel
+    {
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
+        public string NewPassword { get; set; }
+    }
+}
