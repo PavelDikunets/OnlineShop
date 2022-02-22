53856ade9a800e0166173b387288894cfd45cde3
DeveloperCramp <biodubstep@gmail.com>
2022-02-22 11:47:27 +0300

    Исправление дублирования продуктов

--- OnlineShop/OnlineShop.Db/Migrations/20220212105808_init.cs
+++ OnlineShop/OnlineShop.Db/Migrations/20220212105808_init.cs
@@ -22,21 +22,8 @@ namespace OnlineShop.Db.Migrations
                 keyColumn: "Id",
                 keyValue: new Guid("e0137362-4ab1-4d30-b56f-4aaa904ab5c9"));
 
-            migrationBuilder.InsertData(
-                table: "Products",
-                columns: new[] { "Id", "Cost", "Description", "ImagePath", "Name" },
-                values: new object[] { new Guid("d875d83e-a324-4ac1-a0ec-323f019c02b3"), 79900m, "Процессор и память:\nПроцессор: Intel i5-8250\nКоличество ядер: Quad-Core\nЧастота: 3.4 ГГц\nОперативная память: 8 Гб\nВидеокарта: GeForce MX150 VRAM GDDR5 объемом 2 ГБ\n\nДисплей:\nДиагональ экрана: 13.3\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 210,9\nТолщина (мм): 14,8\nДлина (мм): 309,6\nВес (г): 1300", "/img/xiaomi_mi_notebook_air_13_3.jpg", "Ноутбук Xiaomi Mi Notebook Air 13.3\"" });
-
-            migrationBuilder.InsertData(
-                table: "Products",
-                columns: new[] { "Id", "Cost", "Description", "ImagePath", "Name" },
-                values: new object[] { new Guid("99629c45-a5f5-4c84-b192-3f49b163e3c0"), 81990m, "Процессор и память:\nПроцессор: Intel I5-1135G7\nКоличество ядер: Quad-Core\nЧастота: 2.4 ГГц\nОперативная память: 8 Гб\nВидеокарта: Intel Iris Xe Graphics\n\nДисплей:\nДиагональ экрана: 13.3\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 210,6\nТолщина (мм): 16,9\nДлина (мм): 309,9\nВес (г): 1200", "/img/asus_expertbook_B5.png", "Ноутбук ASUS ExpertBook B5 13.3\"" });
-
-            migrationBuilder.InsertData(
-                table: "Products",
-                columns: new[] { "Id", "Cost", "Description", "ImagePath", "Name" },
-                values: new object[] { new Guid("e43a1f28-bdfc-460d-9e7a-493aca649d0b"), 35990m, "Процессор и память:\nКоличество ядер процессора: Dual-Core\nЧастота: 3.0ГГц\nОперативная память: 8 Гб\nВидеокарта: Intel UHD Graphics\n\nДисплей:\nДиагональ экрана: 15.6\nРазрешение дисплея: 1920 x 1080\n\nВнешний вид:\nШирина (мм): 363.96\nТолщина (мм): 19\nДлина (мм): 249\nВес (г): 1780", "/img/dell_vostro_3500_15_6.jpg", "Ноутбук DELL Vostro 3500, 15.6\"" });
         }
+
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
