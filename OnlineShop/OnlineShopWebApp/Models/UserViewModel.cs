16b48c668851720d089b9a4bb0217c3a1a8f3c77
DeveloperCramp <biodubstep@gmail.com>
2022-02-26 11:12:11 +0300

    Rename UserAccountViewModel to UserViewModel

--- OnlineShop/OnlineShopWebApp/Models/UserViewModel.cs
+++ OnlineShop/OnlineShopWebApp/Models/UserViewModel.cs
@@ -3,13 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShopWebApp.Models
 {
-    public class UserAccountViewModel
+    public class UserViewModel
     {
         public string Id { get; set; }
 
         [Required(ErrorMessage = "Заполните поле")]
         [EmailAddress(ErrorMessage = "E-mail указан неверно")]
-        public string Login { get; set; }
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Заполните поле")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
