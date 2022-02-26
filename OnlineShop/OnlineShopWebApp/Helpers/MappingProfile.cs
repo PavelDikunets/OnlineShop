377e8e1249078797a3dee994375b319e83741f23
DeveloperCramp <biodubstep@gmail.com>
2022-02-26 11:13:37 +0300

    Config MappingProfile

---
+++ OnlineShop/OnlineShopWebApp/Helpers/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using OnlineShop.Db.Models;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserViewModel>();
+            CreateMap<Product, ProductViewModel>();
+            CreateMap<Cart, CartViewModel>();
+            CreateMap<Order, OrderViewModel>();
+            CreateMap<UserDeliveryInfo, UserDeliveryInfoViewModel>();
+            CreateMap<CartItem, CartItemViewModel>();
+        }
+    };
+}
