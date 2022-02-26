7e16d2cb3d54ab46b7db6832e99588cafd8ee64a
DeveloperCramp <biodubstep@gmail.com>
2022-02-26 17:30:33 +0300

    Login -> UserName

--- OnlineShop/OnlineShopWebApp/Areas/Admin/Models/ChangePasswordViewModel.cs
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Models/ChangePasswordViewModel.cs
@@ -4,7 +4,7 @@ namespace OnlineShopWebApp.Areas.Admin.Models
 {
     public class ChangePasswordViewModel
     {
-        public string Login { get; set; }
+        public string UserName { get; set; }
 
         [Required(ErrorMessage = "Заполните поле")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
