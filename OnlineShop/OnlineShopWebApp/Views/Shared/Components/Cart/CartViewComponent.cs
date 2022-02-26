d5b4c6d64718c3a8c10b7258e89edd1ba2e541fa
DeveloperCramp <biodubstep@gmail.com>
2022-02-26 17:50:45 +0300

    Added AutoMapper

--- OnlineShop/OnlineShopWebApp/Views/Shared/Components/Cart/CartViewComponent.cs
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -1,24 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Db;
-using OnlineShopWebApp.Helpers;
 using System.Threading.Tasks;
 
-namespace OnlineShopWebApp.Views.Shared.Components.Cart
+namespace OnlineShopWebApp.Views.Shared.Components.CartToViewModel
 {
     public class CartViewComponent : ViewComponent
     {
         private readonly ICartsStorage cartsStorage;
+        private readonly IMapper _mapper;
 
-        public CartViewComponent(ICartsStorage cartsStorage)
+        public CartViewComponent(ICartsStorage cartsStorage, IMapper mapper)
         {
             this.cartsStorage = cartsStorage;
+            _mapper = mapper;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var cart = await cartsStorage.TryGetByUserIdAsync(Constants.UserId);
-            var cartViewModel = cart.ToCartViewModel();
-            var productCounts = cartViewModel?.Amount ?? 0;
+            var cartModel = _mapper.Map<CartViewModel>(cart);
+            var productCounts = cartModel?.Amount ?? 0;
             return View("Cart", productCounts);
         }
     }
