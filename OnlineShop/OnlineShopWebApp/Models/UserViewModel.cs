fae6a207541959729839d7ba5ad1d5175b19bff4
DeveloperCramp <biodubstep@gmail.com>
2022-02-27 12:59:20 +0300

    Add ReqularExpression for UserName Delete FirstName, LastName

--- OnlineShop/OnlineShopWebApp/Models/UserViewModel.cs
+++ OnlineShop/OnlineShopWebApp/Models/UserViewModel.cs
@@ -5,8 +5,12 @@ namespace OnlineShopWebApp.Models
     public class UserViewModel
     {
         [Required(ErrorMessage = "Заполните поле")]
-        [EmailAddress(ErrorMessage = "E-mail указан неверно")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Логин не может быть меньше 4 символов")]
+        [RegularExpression(@"[a-z,A-Z,0-9]+$", ErrorMessage = "Логин может состоять только из цифр и букв английской раскладки")]
         public string UserName { get; set; }
+
+        [EmailAddress(ErrorMessage = "E-mail указан неверно")]
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Заполните поле")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль не может быть меньше 6 символов")]
@@ -15,14 +19,6 @@ namespace OnlineShopWebApp.Models
         [Required(ErrorMessage = "Заполните поле")]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
-
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "Имя не может быть меньше двух букв")]
-        [RegularExpression(@"[а-яА-Я-]+$", ErrorMessage = "Пожалуйста, используйте кириллицу для указания имени")]
-        public string FirstName { get; set; }
-
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "Фамилия не может быть меньше двух букв")]
-        [RegularExpression(@"[а-яА-Я-]+$", ErrorMessage = "Пожалуйста, используйте кириллицу для указания фамилии")]
-        public string LastName { get; set; }
 
         [RegularExpression(@"\+7\s[0-9]{3}\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$", ErrorMessage = "Введите номер телефона в формате: +7 XXX XXX-XX-XX")]
         public string PhoneNumber { get; set; }
