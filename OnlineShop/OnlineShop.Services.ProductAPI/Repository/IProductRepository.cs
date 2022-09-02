6ae3b5a064001f45b10131502d5afff6f71f0e65
DeveloperCramp <biodubstep@gmail.com>
2022-09-02 11:49:15 +0300

    Add Interface IProductRepository

---
+++ OnlineShop/OnlineShop.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShop.Services.ProductAPI.Models.Dtos;
+
+namespace OnlineShop.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(Guid productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(Guid productId);
+    }
+}
