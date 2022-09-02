bcb4ae9835d4427e6882d10edffedfc0233fb628
DeveloperCramp <biodubstep@gmail.com>
2022-09-02 11:45:37 +0300

    Add ProductDto

---
+++ OnlineShop/OnlineShop.Services.ProductAPI/Models/Dtos/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Services.ProductAPI.Models.Dtos
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
