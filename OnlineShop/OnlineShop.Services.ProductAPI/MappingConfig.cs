6c75720774ef354dc891e82dd3b9ba0465e28668
DeveloperCramp <biodubstep@gmail.com>
2022-09-02 12:09:53 +0300

    Add MappingConfig

---
+++ OnlineShop/OnlineShop.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OnlineShop.Services.ProductAPI.Models;
+using OnlineShop.Services.ProductAPI.Models.Dtos;
+
+namespace OnlineShop.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var MappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return MappingConfig;
+        }
+    }
+}
