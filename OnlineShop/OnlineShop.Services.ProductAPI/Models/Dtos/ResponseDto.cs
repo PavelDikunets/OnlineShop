527f2594f9c53d18c8fe0913dd0b76a1cafeca91
DeveloperCramp <biodubstep@gmail.com>
2022-09-02 14:09:04 +0300

    Rename mistake

--- OnlineShop/OnlineShop.Services.ProductAPI/Models/Dtos/ResponseDto.cs
+++ OnlineShop/OnlineShop.Services.ProductAPI/Models/Dtos/ResponseDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace OnlineShop.Services.ProductAPI.Models.Dtos
 {
-    public class ResonseDto
+    public class ResponseDto
     {
         public bool IsSuccess { get; set; } = true;
         public object Result { get; set; }
