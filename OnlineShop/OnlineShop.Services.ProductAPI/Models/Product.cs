5b058848dd8cf4e2ee22cb93d84f993f2f75b569
DeveloperCramp <biodubstep@gmail.com>
2022-09-02 11:36:06 +0300

    Add model Product

---
+++ OnlineShop/OnlineShop.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 999999)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
