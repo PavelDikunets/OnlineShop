dff9d198ca5ba29ade57d08bb48ec769d50cc2ed
DeveloperCramp <biodubstep@gmail.com>
2022-09-02 11:45:44 +0300

    Add ResponseDto

---
+++ OnlineShop/OnlineShop.Services.ProductAPI/Models/Dtos/ResonseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShop.Services.ProductAPI.Models.Dtos
+{
+    public class ResonseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
