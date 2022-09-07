334593ff2a58f89a2ad3be74b71db5693cc92a41
Pavel Dikunets <biodubstep@gmail.com>
2022-09-07 12:21:15 +0300

    Add DbSet Products

--- OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Services.ProductAPI.Models;
 
 namespace OnlineShop.Services.ProductAPI.DbContexts
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
