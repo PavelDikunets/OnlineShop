4e28d7649d71fc96b60382822a9aafb4103ca5ff
Pavel Dikunets <biodubstep@gmail.com>
2022-09-07 14:36:36 +0300

    Add mapping config

---
+++ OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OnlineShop.Services.ProductAPI.Models;
+using OnlineShop.Services.ProductAPI.Models.Dto;
+
+namespace OnlineShop.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Product, ProductDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
