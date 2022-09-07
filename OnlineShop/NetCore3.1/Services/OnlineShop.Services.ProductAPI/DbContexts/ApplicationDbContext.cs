d866f7491c68ab2cd55a4e8a82da7ca8e687ef1a
Pavel Dikunets <biodubstep@gmail.com>
2022-09-07 11:59:39 +0300

    Add blank ApplicationDbContext

---
+++ OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
