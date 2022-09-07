ebe4c90ed508dad6c5039f3fe163d6ccad03326f
Pavel Dikunets <biodubstep@gmail.com>
2022-09-07 12:21:25 +0300

    Add model Product

---
+++ OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace OnlineShop.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1,999999)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
