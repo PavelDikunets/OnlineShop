90cc10309934a36ec1a8c06347d38175898163e2
Pavel Dikunets <biodubstep@gmail.com>
2022-09-08 16:01:39 +0300

    - Add HttpGet by ProductId - Add HttpPut - Add HttpPost - Add HttpDelete

--- OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ OnlineShop/NetCore3.1/Services/OnlineShop.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -36,5 +36,73 @@ namespace OnlineShop.Services.ProductAPI.Controllers
             return _response;
         }
 
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ResponseDto> Get(Guid id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<ResponseDto> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<ResponseDto> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public async Task<ResponseDto> Delete(Guid id)
+        {
+            try
+            {
+                bool isSuccess = await _productRepository.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
     }
 }
