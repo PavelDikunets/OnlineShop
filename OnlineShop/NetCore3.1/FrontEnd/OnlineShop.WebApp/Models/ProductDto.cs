5aa668334d626ae4e27c461f4756be9282fe6fd2
Pavel Dikunets <biodubstep@gmail.com>
2022-09-09 12:30:39 +0300

    Add ProductDto model

---
+++ OnlineShop/NetCore3.1/FrontEnd/OnlineShop.WebApp/Models/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OnlineShop.WebApp.Models
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
