8814eb9b8d973b8792312f04d29f87ef69687e86
Pavel Dikunets <biodubstep@gmail.com>
2022-09-09 12:58:03 +0300

    Add Interface IBaseService

---
+++ OnlineShop/NetCore3.1/FrontEnd/OnlineShop.WebApp/Services/IServices/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿
+using OnlineShop.WebApp.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlineShop.WebApp.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
