3f8ef7e84b9c2654e331241bed85f7ae3fc49c34
Pavel Dikunets <biodubstep@gmail.com>
2022-09-09 12:57:46 +0300

    Add ApiRequest

---
+++ OnlineShop/NetCore3.1/FrontEnd/OnlineShop.WebApp/Models/ApiRequest.cs
@@ -0,0 +1,11 @@
+﻿using static OnlineShop.WebApp.StaticDetails;
+
+namespace OnlineShop.WebApp.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
