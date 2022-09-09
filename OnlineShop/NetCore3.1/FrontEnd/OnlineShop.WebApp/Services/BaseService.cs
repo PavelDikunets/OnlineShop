d3747a64f5ef2b89b43904e1751f553e35e0f571
Pavel Dikunets <biodubstep@gmail.com>
2022-09-09 12:58:46 +0300

    Implement BaseService for API responses

---
+++ OnlineShop/NetCore3.1/FrontEnd/OnlineShop.WebApp/Services/BaseService.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using OnlineShop.WebApp.Models;
+using OnlineShop.WebApp.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.WebApp.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            responseModel = new ResponseDto();
+            this.httpClient = httpClient;
+        }
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("OnlineShopAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case StaticDetails.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case StaticDetails.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case StaticDetails.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+                var apiContet = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContet);
+                return apiResponseDto;
+            }
+            catch (Exception ex)
+            {
+
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDto;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
