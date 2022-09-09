b94d437c2eec6656ef30d91f8911f5f646099800
Pavel Dikunets <biodubstep@gmail.com>
2022-09-09 13:43:49 +0300

    Implement ProductService

---
+++ OnlineShop/NetCore3.1/FrontEnd/OnlineShop.WebApp/Services/ProductService.cs
@@ -0,0 +1,65 @@
+﻿using OnlineShop.WebApp.Models;
+using OnlineShop.WebApp.Services.IServices;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineShop.WebApp.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductService(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> CreateProductAsync<T>(ProductDto productDto)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = productDto,
+                Url = StaticDetails.ProductAPIBase + "/api/products/"
+            });
+        }
+
+        public async Task<T> DeleteProductAsync<T>(Guid id)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.DELETE,
+                Url = StaticDetails.ProductAPIBase + "/api/products/" + id
+            });
+        }
+
+        public async Task<T> GetAllProductsAsync<T>()
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.ProductAPIBase + "/api/products/"
+            });
+        }
+
+        public async Task<T> GetProductbyIdAsync<T>(Guid id)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.ProductAPIBase + "/api/products/" + id
+            });
+        }
+
+        public async Task<T> UpdateProductAsync<T>(ProductDto productDto)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.PUT,
+                Data = productDto,
+                Url = StaticDetails.ProductAPIBase + "/api/products/"
+            });
+        }
+    }
+}
