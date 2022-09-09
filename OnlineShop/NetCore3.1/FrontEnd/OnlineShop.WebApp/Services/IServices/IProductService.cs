5c45a55ab852fbf9f6356468b10eb5d51e1b7557
Pavel Dikunets <biodubstep@gmail.com>
2022-09-09 12:31:47 +0300

    Add IProductService with signatures: - GetAllProductsAsync - GetProductbyIdAsync - CreateProductAsync - UpdateProductAsync - DeleteProductAsync

---
+++ OnlineShop/NetCore3.1/FrontEnd/OnlineShop.WebApp/Services/IServices/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using OnlineShop.WebApp.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlineShop.WebApp.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductbyIdAsync<T>(Guid id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(Guid id);
+    }
+}
